df00a4ae2b7690b9db31ea35b4267e18b4fc5c0e
Guilherme Mendes da Cunha <60897048+firgo12@users.noreply.github.com>
2024-11-18 09:37:17 -0300

    Subindo os dataLayers

--- E Store Free Website Template - Free-CSS.com/ecommerce-html-template/js/main.js
+++ E Store Free Website Template - Free-CSS.com/ecommerce-html-template/js/main.js
@@ -237,3 +237,492 @@
     });
 })(jQuery);
 
+//Função do page view 
+function getPageView() {
+    window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'page_view',
+            page_title:  document.body.getAttribute('gtmName'),
+            page_location: document.body.getAttribute('gtmLocation')       
+        });
+}
+
+// Conjunto para armazenar os valores únicos gerados
+const valoresGerados = new Set();
+
+// Função do evento Sign Up
+function getSignUp() {
+    let valor;
+    
+    do {
+        // Gerar um valor aleatório com 3 caracteres (letras e números)
+        valor = Math.random().toString(36).substring(2, 5); // 36 base (alfabético + numérico) e pega 3 caracteres após o ponto decimal
+    } while (valoresGerados.has(valor)); // Verifica se o valor já foi gerado
+
+    // Adiciona o valor gerado ao conjunto
+    valoresGerados.add(valor);
+
+    window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'sign_up',
+            user_id: valor,       
+        });
+}
+
+// Função do evento Log in
+function getLogIn() {
+    let valor;
+    
+    do {
+        // Gerar um valor aleatório com 3 caracteres (letras e números)
+        valor = Math.random().toString(36).substring(2, 5); // 36 base (alfabético + numérico) e pega 3 caracteres após o ponto decimal
+    } while (valoresGerados.has(valor)); // Verifica se o valor já foi gerado
+
+    // Adiciona o valor gerado ao conjunto
+    valoresGerados.add(valor);
+
+    window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'login',
+            user_id: valor,       
+        });
+}
+
+// Função do evento Select Content
+function getSelectContent() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'select_content',
+        ev_action:  document.body.getAttribute('gtmEvAction'),
+        ev_label: document.body.getAttribute('gtmEvLabel')     
+    });
+}
+
+// Função do evento view Item List
+function getViewItemList() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "view_item_list",
+        item_list_id: "1",
+        item_list_name: "clothes",
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "product_1",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black"
+                },
+                {
+                    item_id: "sku_12346",
+                    item_name: "product_2",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue"
+                },
+                {
+                    item_id: "sku_12347",
+                    item_name: "product_3",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue"
+                },
+                {
+                    item_id: "sku_12348",
+                    item_name: "product_4",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:red"
+                },
+                {
+                    item_id: "sku_12349",
+                    item_name: "product_5",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue"
+                },
+                {
+                    item_id: "sku_12350",
+                    item_name: "product_6",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:gray"
+                },
+                {
+                    item_id: "sku_12351",
+                    item_name: "product_7",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black"
+                },
+                {
+                    item_id: "sku_12352",
+                    item_name: "product_8",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black"
+                },
+                {
+                    item_id: "sku_12353",
+                    item_name: "product_9",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:gray"
+                },
+            ]
+        }
+    });
+}
+
+//Função do evento view Item
+function getViewItem() {
+    let name = document.getElementById('productName').querySelector('h2').textContent;
+
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'view_item',
+        currency: 'BRL',
+        value: parseFloat(document.body.querySelector('.price p').textContent.trim().split(' ')[0].replace('$', '')),
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: name,
+                    item_category: "clothes",
+                    price: parseFloat(document.body.querySelector('.price p').textContent.trim().split(' ')[0].replace('$', '')),
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                }
+            ]
+        }      
+    });
+}
+
+//Função do evento add to cart
+function getAddCart() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'add_to_cart',
+        currency: 'BRL',
+        value: 99,
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "Product name",
+                    item_category: "clothes",
+                    price: 100,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                }
+            ]
+        }      
+    });
+}
+
+//Função do evento remove from cart
+function getRemoveCart() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'remove_from_cart',
+        currency: 'BRL',
+        value: 99,
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "Product name",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                }
+            ]
+        }      
+    });
+}
+
+//Função do evento view cart
+function getViewCart() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "view_cart",
+        currency: "BRL",
+        value: 100,
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "product_1",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                },
+                {
+                    item_id: "sku_12346",
+                    item_name: "product_2",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:xl"
+                },
+                {
+                    item_id: "sku_12347",
+                    item_name: "product_3",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:l"
+                },
+                {
+                    item_id: "sku_12348",
+                    item_name: "product_4",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:red:m"
+                },
+                {
+                    item_id: "sku_12349",
+                    item_name: "product_5",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:s"
+                }
+            ]
+        }
+    });
+}
+
+//Função do evento checkout
+function getCheckout() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "begin_checkout",
+        currency: "BRL",
+        value: 100,
+        payment_type: "Paypal",
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "product_1",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                },
+                {
+                    item_id: "sku_12346",
+                    item_name: "product_2",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:xl"
+                },
+                {
+                    item_id: "sku_12347",
+                    item_name: "product_3",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:l"
+                },
+                {
+                    item_id: "sku_12348",
+                    item_name: "product_4",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:red:m"
+                },
+                {
+                    item_id: "sku_12349",
+                    item_name: "product_5",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:s"
+                }
+            ]
+        }
+    });
+}
+
+//Função do evento add payment info
+function getAddPaymentInfo(paymentType) {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'add_payment_info',
+        ev_action: 'click:payment_info:sucesso',
+        ev_label: 'Enviado com sucesso',
+        currency: 'BRL',
+        value: 99,
+        payment_type: paymentType,
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "Product name",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                }
+            ]
+        }      
+    });
+}
+
+let payment;
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona todos os inputs do tipo radio dentro da classe payment-methods
+    const paymentInputs = document.querySelectorAll('.payment-methods .custom-control-input');
+    
+    // Adiciona um evento de clique para cada input
+    paymentInputs.forEach(function(input) {
+        input.addEventListener('click', function() {
+            
+            // Captura o valor do atributo gtmName
+            let paymentType = input.getAttribute('gtmName');
+            
+            // Verifica se o atributo gtmName está presente
+            if (paymentType) {
+                payment = paymentType
+            } else {
+                console.error('Atributo gtmName não encontrado para este input.');
+            }
+        });
+    });
+});
+
+
+
+//Função do evento add shipping info
+function getAddShippingInfo() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'add_shipping_info',
+        ev_action: 'click:shipping_info:sucesso',
+        ev_label: 'Enviado com sucesso',
+        currency: 'BRL',
+        value: 100,
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "product_1",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                },
+                {
+                    item_id: "sku_12346",
+                    item_name: "product_2",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:xl"
+                },
+                {
+                    item_id: "sku_12347",
+                    item_name: "product_3",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:l"
+                },
+                {
+                    item_id: "sku_12348",
+                    item_name: "product_4",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:red:m"
+                },
+                {
+                    item_id: "sku_12349",
+                    item_name: "product_5",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:s"
+                }
+            ]
+        }
+    });
+}
+
+//Função do evento purchase
+function getPurchase(){
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'purchase',
+        currency: 'BRL',
+        value: 100,
+        transaction_id: Math.floor(Math.random() * 100).toString(),
+        shipping: Math.floor(Math.random() * 100),
+        ecommerce: {
+            items: [{
+                    item_id: "sku_12345",
+                    item_name: "product_1",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:black:s"
+                },
+                {
+                    item_id: "sku_12346",
+                    item_name: "product_2",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:xl"
+                },
+                {
+                    item_id: "sku_12347",
+                    item_name: "product_3",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:l"
+                },
+                {
+                    item_id: "sku_12348",
+                    item_name: "product_4",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:red:m"
+                },
+                {
+                    item_id: "sku_12349",
+                    item_name: "product_5",
+                    item_category: "clothes",
+                    price: 99,
+                    quantity: 1,
+                    item_variant: "color:blue:s"
+                }
+            ]
+        }
+    });
+}
+
+
+const btnOrder = document.getElementById('btn-order');
+
+btnOrder.addEventListener('click', function(event) {
+    
+    getAddPaymentInfo(payment);
+    getAddShippingInfo();
+
+    if(payment != undefined){
+        getPurchase();
+    }
+});
